17d33e7d99467708d90eadc05049d5047bf06615
Mario Tijerino <mariogotijerino@gmail.com>
2020-09-30 17:51:02 -0600

    Transferred files to repo

---
+++ Unity Projects/Walk_Run/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 10f, sprintModifier = 2f;
+    private Rigidbody rb;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        var hInput = Input.GetAxis("Horizontal");
+        var vInput = Input.GetAxis("Vertical");
+        var movement = new Vector3(hInput,0,vInput);
+
+        var speedModifier = 1f;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speedModifier *= sprintModifier;
+        }
+        
+        movement = Vector3.ClampMagnitude(movement, 1);
+        rb.MovePosition(transform.position + movement * (moveSpeed * speedModifier * Time.fixedDeltaTime));
+    }
+}
