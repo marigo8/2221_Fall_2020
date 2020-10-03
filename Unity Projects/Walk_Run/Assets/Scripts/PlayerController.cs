d107c632516c0f059d4b72af0028ca02c187bd3b
Mario Tijerino <mariogotijerino@gmail.com>
2020-10-03 15:41:37 -0600

    Added comments, made rotateSpeed & speedModifier properties

--- Unity Projects/Walk_Run/Assets/Scripts/PlayerController.cs
+++ Unity Projects/Walk_Run/Assets/Scripts/PlayerController.cs
@@ -6,33 +6,40 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f, sprintModifier = 2f;
+
+    private float rotateSpeed = 10f, speedModifier;
     private Rigidbody rb;
     
     private void Start()
     {
+        // Get Components
         rb = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
+        // Input
         var hInput = Input.GetAxis("Horizontal");
         var vInput = Input.GetAxis("Vertical");
         var movement = new Vector3(hInput,0,vInput);
 
-        var speedModifier = 1f;
-
+        // Sprint
+        speedModifier = 1f;
         if (Input.GetKey(KeyCode.LeftShift))
         {
             speedModifier *= sprintModifier;
         }
         
+        // Move Player
         movement = Vector3.ClampMagnitude(movement, 1);
         rb.MovePosition(transform.position + movement * (moveSpeed * speedModifier * Time.fixedDeltaTime));
 
+        // Rotate Player
         if (movement.magnitude > 0)
         {
             var rotation = Quaternion.LookRotation(movement.normalized);
-            rb.MoveRotation(Quaternion.Lerp(transform.rotation, rotation, 10f * Time.deltaTime));
+            rotation = Quaternion.Lerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
+            rb.MoveRotation(rotation);
         }
     }
 }
