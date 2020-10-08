a6e1bef044f892b98c94f6bb8d5d082b73ad931e
Mario Tijerino <mariogotijerino@gmail.com>
2020-10-07 18:28:09 -0600

    Merged Walk_Run and Jump_DoubleJump

--- Unity Projects/Sandbox/Assets/Scripts/PlayerMoveBehaviour.cs
+++ Unity Projects/Sandbox/Assets/Scripts/PlayerMoveBehaviour.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMoveBehaviour : MonoBehaviour
 {
-    public float moveSpeed = 5f, sprintModifier = 2f;
+    public IntData jumpCount;
+    public float moveSpeed = 5f, sprintModifier = 2f, jumpStrength;
     private float rotateSpeed = 10f, speedModifier;
 
     private Vector3 forces = Vector3.zero;
     
     private CharacterController controller;
-    
+    private Vector3 movement;
+
     private void Start()
     {
         // Get Components
@@ -20,10 +22,30 @@ public class PlayerMoveBehaviour : MonoBehaviour
 
     private void Update()
     {
+        Gravity();
+        WalkRun();
+        Jump();
+        
+        // Movement
+        controller.Move((movement + forces) * Time.deltaTime);
+    }
+
+    private void Gravity()
+    {
+        // Gravity
+        if (controller.velocity.y < 0) // This fixes issue where player halts at the peak of the jump.
+        {
+            forces.y = controller.velocity.y;
+        }
+        forces.y += Physics.gravity.y * Time.deltaTime;
+    }
+
+    private void WalkRun()
+    {
         // Input
         var hInput = Input.GetAxis("Horizontal");
         var vInput = Input.GetAxis("Vertical");
-        var movement = new Vector3(hInput, 0, vInput);
+        movement = new Vector3(hInput, 0, vInput);
 
         // Sprint
         speedModifier = 1f;
@@ -31,20 +53,10 @@ public class PlayerMoveBehaviour : MonoBehaviour
         {
             speedModifier *= sprintModifier;
         }
-        
-        // Gravity
-        if (controller.velocity.y < 0)
-        {
-            forces.y = controller.velocity.y;
-        }
-        forces.y += Physics.gravity.y * Time.deltaTime;
-        Debug.Log(controller.isGrounded);
 
         // Move Player
         movement = Vector3.ClampMagnitude(movement, 1);
         movement *= moveSpeed * speedModifier;
-        
-        controller.Move((movement + forces) * Time.deltaTime);
 
         // Rotate Player
         if (movement.magnitude > 0)
@@ -52,6 +64,22 @@ public class PlayerMoveBehaviour : MonoBehaviour
             var rotation = Quaternion.LookRotation(movement.normalized);
             rotation = Quaternion.Lerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
             transform.rotation = rotation;
+        }
+    }
+
+    private void Jump()
+    {
+        // On Grounded
+        if (controller.isGrounded)
+        {
+            jumpCount.SetValue(0);
+        }
+            
+        // Jump
+        if (Input.GetButtonDown("Jump") && !jumpCount.IsMaxed)
+        {
+            forces.y = jumpStrength;
+            jumpCount.AddToValue(1);
         }
     }
 }
