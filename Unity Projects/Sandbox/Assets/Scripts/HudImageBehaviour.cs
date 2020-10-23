5aa801a47c07c5add95457654a1f1e2b195d358f
Mario Tijerino <mariogotijerino@gmail.com>
2020-10-23 12:46:01 -0600

    Slider Health Bar

--- Unity Projects/Sandbox/Assets/Scripts/HudImageBehaviour.cs
+++ Unity Projects/Sandbox/Assets/Scripts/HudImageBehaviour.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Image))]
-public class ScriptableDataImageBehaviour : MonoBehaviour
+public class HudImageBehaviour : MonoBehaviour
 {
     public ScriptableData data;
     public Gradient gradient, backgroundGradient;
---
+++ Unity Projects/Sandbox/Assets/Scripts/HudSliderBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+[RequireComponent(typeof(Slider))]
+public class HudSliderBehaviour : MonoBehaviour
+{
+    public FloatData health, maxHealth;
+    
+    private Slider healthSlider;
+    private RectTransform sliderTransform;
+
+    private void Start()
+    {
+        healthSlider = GetComponent<Slider>();
+        healthSlider.maxValue = maxHealth.value;
+        sliderTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        healthSlider.value = health.value;
+        sliderTransform.sizeDelta = new Vector2(maxHealth.value, sliderTransform.sizeDelta.y);
+    }
+}
