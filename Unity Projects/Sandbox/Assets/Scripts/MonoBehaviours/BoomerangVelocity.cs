035ce8f6189a081ee01fb4ebb977e61e185c1ca3
Mario Tijerino <mariogotijerino@gmail.com>
2020-10-24 11:04:45 -0600

    boomerang script

---
+++ Unity Projects/Sandbox/Assets/Scripts/MonoBehaviours/BoomerangVelocity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomerangVelocity : MonoBehaviour
+{
+    public bool canShoot = true;
+    public Rigidbody rBody;
+    public WaitForFixedUpdate wffu = new WaitForFixedUpdate();
+    public Vector3 initalVelocity; // <---
+
+    public IEnumerator Start()
+    {
+        rBody.velocity = initalVelocity; // <---
+        var originalVelocity = initalVelocity;
+        while (canShoot)
+        {
+            rBody.velocity = Vector3.Lerp(rBody.velocity, -originalVelocity, 1f);
+            yield return wffu;
+            Debug.Log("ack");
+        }
+    }
+}
