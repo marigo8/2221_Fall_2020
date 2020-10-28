34b060593e75e30408338591651c2e016aa24815
Mario Tijerino <mariogotijerino@gmail.com>
2020-10-27 22:25:18 -0600

    Finished Layout

---
+++ Unity Projects/Sandbox/Assets/Scripts/MonoBehaviours/LockCameraHeightBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCameraHeightBehaviour : MonoBehaviour
+{
+    public float height;
+
+    private void LateUpdate()
+    {
+        var position = transform.position;
+        position.y = height;
+        transform.position = position;
+    }
+}
