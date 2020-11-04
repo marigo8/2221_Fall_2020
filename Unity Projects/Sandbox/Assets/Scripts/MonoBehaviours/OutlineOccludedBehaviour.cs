a3fc600de924022cbda49d3ffbbcf36d00087bd7
Mario Tijerino <mariogotijerino@gmail.com>
2020-11-03 17:51:39 -0700

    Outline Player When Occluded

---
+++ Unity Projects/Sandbox/Assets/Scripts/MonoBehaviours/OutlineOccludedBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using cakeslice;
+using UnityEngine;
+
+public class OutlineOccludedBehaviour : MonoBehaviour
+{
+    public Outline playerOutline;
+
+    private void Start()
+    {
+        playerOutline.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out var hit,10f, LayerMask.GetMask("Default")))
+        {
+            Debug.Log(hit.collider.name);
+            playerOutline.enabled = true;
+        }
+        else
+        {
+            playerOutline.enabled = false;
+        }
+    }
+}
