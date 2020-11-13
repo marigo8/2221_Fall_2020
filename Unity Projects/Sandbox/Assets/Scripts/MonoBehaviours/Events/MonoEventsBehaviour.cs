c1515fd05ed788ea5552666f996b59c0fdc8d9de
Mario Tijerino <mariogotijerino@gmail.com>
2020-11-13 14:32:19 -0700

    Ball Container Animation. Player Fall Damage

---
+++ Unity Projects/Sandbox/Assets/Scripts/MonoBehaviours/Events/MonoEventsBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEventsBehaviour : MonoBehaviour
+{
+    public UnityEvent startEvent, updateEvent, enableEvent, destroyEvent;
+
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+
+    private void Update()
+    {
+        updateEvent.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        enableEvent.Invoke();
+    }
+
+    private void OnDestroy()
+    {
+        destroyEvent.Invoke();
+    }
+}
