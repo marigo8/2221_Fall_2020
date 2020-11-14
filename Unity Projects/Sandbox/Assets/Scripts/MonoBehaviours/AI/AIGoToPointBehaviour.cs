a92a9b504fb338a26faf8404d480eb26c23d7356
Mario Tijerino <mariogotijerino@gmail.com>
2020-11-13 21:36:11 -0700

    Added Terrain and Squirrel

---
+++ Unity Projects/Sandbox/Assets/Scripts/MonoBehaviours/AI/AIGoToPointBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIGoToPointBehaviour : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    public void SetTargetPoint(Transform target)
+    {
+        agent.destination = target.position;
+    }
+    
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = transform.position;
+    }
+}
